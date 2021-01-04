67fdc3f90ad9aa11751a211628cbeb1632754ccb
Fernanda Dias <fernandasd26@gmail.com>
2021-01-03 22:20:20 -0300

    Creating the basic structure of the app

---
+++ lib/screens/constants.dart
@@ -0,0 +1,36 @@
+import 'package:shop_app/models/product.dart';
+
+final List<Product> loadedProducts = [
+  Product(
+    id: 'p1',
+    title: 'Red Shirt',
+    description: 'Red Shirt Description',
+    price: 29.99,
+    imageUrl:
+        'https://images-na.ssl-images-amazon.com/images/I/71IrAzV-xJL._AC_UL1500_.jpg',
+  ),
+  Product(
+    id: 'p1',
+    title: 'Red Shirt',
+    description: 'Red Shirt Description',
+    price: 29.99,
+    imageUrl:
+        'https://images-na.ssl-images-amazon.com/images/I/71IrAzV-xJL._AC_UL1500_.jpg',
+  ),
+  Product(
+    id: 'p1',
+    title: 'Red Shirt',
+    description: 'Red Shirt Description',
+    price: 29.99,
+    imageUrl:
+        'https://images-na.ssl-images-amazon.com/images/I/71IrAzV-xJL._AC_UL1500_.jpg',
+  ),
+  Product(
+    id: 'p1',
+    title: 'Red Shirt',
+    description: 'Red Shirt Description',
+    price: 29.99,
+    imageUrl:
+        'https://images-na.ssl-images-amazon.com/images/I/71IrAzV-xJL._AC_UL1500_.jpg',
+  )
+];
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageURL;
+
+  ProductItem(this.id, this.title, this.imageURL);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        this.imageURL,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
